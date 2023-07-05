f517cd96da778ca2edc09ace51675ed8f9027f0f
Drolpg <leandronascimento.drolpg21@gmail.com>
2023-07-05 16:03:53 -0300

    Start project

---
+++ app/src/main/java/com/ost_dro/pokedex_dro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ost_dro.pokedex_dro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
